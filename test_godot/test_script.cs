3ce1dcb724ec86adb4b3a80a7c0c77fc305cc510
hoel45 <MrPvP2.minecraft@gmail.com>
2022-11-19 18:09:33 +0100

    modification script test

--- test_godot/test_script.cs
+++ test_godot/test_script.cs
@@ -3,19 +3,11 @@ using System;
 
 public class test_script : Node
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+ 
+	public override void _Ready()
+	{
+		
+	}
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
