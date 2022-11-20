efc344861cd93469b11d4bea386afb4b37655aeb
gogolum <74010058+gogolum@users.noreply.github.com>
2022-11-20 03:05:13 +0100

    new character plugin
    -character movement

--- test_godot/scripts/Character_Controller.cs
+++ test_godot/scripts/Character_Controller.cs
@@ -8,14 +8,37 @@ public class Character_Controller : Sprite
 	// private string b = "text";
 
 	// Called when the node enters the scene tree for the first time.
+
+	public Vector2 ScreenSize;
+
 	public override void _Ready()
 	{
-		
+		ScreenSize = GetViewportRect().Size;
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	public override void _Process(float delta)
+	{
+		float speed = 10;
+
+		if (Input.IsActionPressed("move_up"))
+		{
+			this.Position += new Vector2(0,-speed);
+		}
+		if (Input.IsActionPressed("move_down"))
+		{
+			this.Position += new Vector2(0,speed);
+		}
+		if (Input.IsActionPressed("move_left"))
+		{
+			this.Position += new Vector2(-speed,0);
+			this.FlipH = true;
+		}
+		if (Input.IsActionPressed("move_right"))
+		{
+			this.Position += new Vector2(speed,0);
+			this.FlipH = false;
+		}
+		
+	}
 }
