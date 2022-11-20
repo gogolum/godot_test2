79b2c868ebb7eaff9e50ca1911ff1fb5710a9867
gogolum <74010058+gogolum@users.noreply.github.com>
2022-11-20 12:18:12 +0100

    comit

--- test_godot/scripts/Character_Controller.cs
+++ test_godot/scripts/Character_Controller.cs
@@ -9,11 +9,11 @@ public class Character_Controller : Sprite
 
 	// Called when the node enters the scene tree for the first time.
 
-	public Vector2 ScreenSize;
+	
 
 	public override void _Ready()
 	{
-		ScreenSize = GetViewportRect().Size;
+		
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
