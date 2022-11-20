96f3e1fdc1d03ae0f6618f5b3a33e46dc412f3b6
hoel45 <MrPvP2.minecraft@gmail.com>
2022-11-20 18:31:00 +0100

    delta time

--- test_godot/Assets/scripts/Character_Controller.cs
+++ test_godot/Assets/scripts/Character_Controller.cs
@@ -19,7 +19,7 @@ public class Character_Controller : Sprite
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
-		float speed = 10;
+		float speed = 1000 * delta;
 	 int vie = 1;
 	  Vector2 movePlayer = new Vector2(0,0);
 		if (Input.IsKeyPressed((int)KeyList.Z))
