c80446d0bb34218f7d7367be5af8da7d51d6c3ce
gogolum <74010058+gogolum@users.noreply.github.com>
2022-11-21 21:24:41 +0100

    dah

--- test_godot/Assets/scripts/Character_Controller.cs
+++ test_godot/Assets/scripts/Character_Controller.cs
@@ -10,11 +10,12 @@ public class Character_Controller : Sprite
 	// Called when the node enters the scene tree for the first time.
 
 	[Export] public float speed = 1000;
+	PackedScene weapon_scene;
 
 
 	public override void _Ready()
 	{
-		
+		weapon_scene = GD.Load<PackedScene>("res://Weapons.tscn");
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -41,5 +42,9 @@ public class Character_Controller : Sprite
 			this.FlipH = false;
 		}
 		this.Position += movePlayer.Normalized()*player_speed;
+
+		Weapons weapons = (Weapons)weapon_scene.Instance();
+		weapons.Position = Position;
+		weapons.Rotation = Rotation;
 	}
 }
