c6734305a033d26105eb93b761b174733877ca91
gogolum <74010058+gogolum@users.noreply.github.com>
2022-11-21 18:58:09 +0100

    changes

---
+++ test_godot/Assets/scripts/Bullet.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public class Bullet : Node2D
+{
+	[Export]float speed = 400;
+	public float range = 1500;
+	private float distanceTraveled = 0;
+	
+	public override void _Ready()
+	{
+		var area = GetNode<Area2D>("Area2D");
+		area.Connect("area_entered", this,"OnCollision");
+		area.Connect("body_entered", this,"OnCollision");
+	}
+
+
+ public override void _Process(float delta)
+ {
+	 float MoveAmount = speed * delta;
+	 Position += Transform.x.Normalized() * MoveAmount;
+	 distanceTraveled += MoveAmount;
+	 if (distanceTraveled > range){
+		QueueFree();
+	 }
+ }
+
+ private void OnCollision(Node with){
+	GD.Print("bullet touched !");
+	QueueFree();
+ }
+}
